8fe65b19a4790523fc136ddfe40ac12e7cdac58a
shaman <27231583@qq.com>
2012-12-28 00:06:52 +0800

    resolved Kindeditor and add compelete ejs templates

--- config.js
+++ config.js
@@ -8,8 +8,8 @@
 
 module.exports = {
     staticPath: 'public',
-    viewEngine: 'jade',
-    viewsDir: __dirname + '/views/jade_tpl',
+    viewEngine: 'ejs',
+    viewsDir: __dirname + '/views/ejs_tpl',
     port: 3000,
     dbSettings: {
         cookieSecret: 'shaman',
