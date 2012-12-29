fb1427a67bf354d11e5dfbcb52543b60b9edeb36
shaman <27231583@qq.com>
2012-12-29 11:09:11 +0800

    ..

--- models/db.js
+++ models/db.js
@@ -1,7 +1,7 @@
 //mongodb数据库的连接
 
 var config = require('../config');
-var mongo = require('mongodb');
+var mongo = require('../node_modules/mongodb');
 var Db = mongo.Db;
 var Connection = mongo.Connection;
 var Server = mongo.Server;
