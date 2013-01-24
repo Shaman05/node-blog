d272e6265d65f705e74e004d8857c9dbafa19a24
shaman <27231583@qq.com>
2013-01-24 11:41:38 +0800

    minor change

--- models/socket.js
+++ models/socket.js
@@ -7,11 +7,9 @@
  */
 var socket = require('socket.io');
 function socketStart(server){
-    var userlist = [];
     socket.listen(server).on('connection', function (socket) {
         //console.log(socket.id + " has been connected!");
         socket.on('message', function (msg) {
-            console.log(typeof msg);
             var d = JSON.parse(msg),
                 sendData = {
                     message : ''
@@ -20,21 +18,17 @@ function socketStart(server){
                 sendData.message = '<em>系统消息:</em><span>【' + d.name + '】</span>加入了聊天室。';
             }
             if(d.type === "message"){
-                //console.log(d.message)
-                //var regScript = /<script.*?>(\s|.)*?<\/script>/i;  //脚本过滤表达式
                 var string = d.message.replace(/</ig, "&lt;");
                 string = string.replace(/>/ig, "&gt;");
                 string = string.replace(/\n/ig, "<br>");
                 string = string.replace(/ /ig, "&nbsp;");
-                sendData.message = '<span>【' + d.name + '】</span>：<div class="txt">' + string + '</div>';
+                sendData.message = '<span>【' + d.name + '】</span><div class="txt">' + string + '</div>';
             }
             socket.broadcast.emit('message', JSON.stringify(sendData));
         });
         socket.on('disconnect', function(){
-            for(var i = 0, len = userlist.length; i < len; i++){
-            }
             //console.log(socket.id + " has been disconnected!");
-        })
+        });
     });
 }
 exports.socketStart = socketStart;
--- public/js/chat.js
+++ public/js/chat.js
@@ -12,27 +12,15 @@
         name = null;
     var chatContent = $(".chatcontent"),
         connInfo = $("#conn-info"),
-        userList = $("#userlist"),
         chatList = $("#chatlist"),
         sendBtn = $("#send"),
         dataArea = $("#senddata");
-
-    $("#name").bind({
-        "blur" : function(){
-            var val = $(this).val();
-            $(this).val(val==""?defaultVal:val);
-        },
-        "focus" : function(){
-            var val = $(this).val();
-            $(this).val(val==defaultVal?"":val);
-        }
-    })
 
     $("#enter").click(function(){
         var iptVal = $.trim($("#name").val());
         name = !!iptVal ? iptVal : null;
         name && name != defaultVal && goChat();
-    })
+    });
 
     function goChat(){
         $("#setname").hide().empty();
@@ -42,31 +30,24 @@
         var data = {
             type : "join",
             name : name
-        }
+        };
 
         socket.on("connect", function(){
             socket.send(JSON.stringify(data));
             connInfo.text("已连接。");
             dataArea.attr("disabled", false);
+        });
 
-            socket.on("message", function(data){
-                var d = JSON.parse(data);
-                chatList.append($('<li>' + d.message + '</li>'));
-                //更新聊天用户列表
-                /*if(d.userList.status == 1){
-                    userList.empty();
-                    var users = d.userList.users;
-                    for(var i = 0, len = users.length; i < len; i++)
-                        userList.append('<li>' + users[i].name + '</li>');
-                }*/
-                setScrollBar();
-            })
+        socket.on("message", function(data){
+            var d = JSON.parse(data);
+            chatList.append($('<li>' + d.message + '</li>'));
+            setScrollBar();
+        });
 
-            socket.on("disconnect", function(){
-                chatList.append($('<li>失去连接。</li>'));
-                sendBtn.unbind();
-            })
-        })
+        socket.on("disconnect", function(){
+            connInfo.text("失去连接。");
+            //sendBtn.unbind();
+        });
 
         sendBtn.click(function(){
             var data = {
@@ -80,21 +61,21 @@
                 string = string.replace(/>/ig, "&gt;");
                 string = string.replace(/\n/ig,"<br>");
                 string = string.replace(/ /ig, "&nbsp;");
-                chatList.append($('<li><strong>你</strong>：<div class="mytxt">' + string + '</div></li>'));
+                chatList.append($('<li><strong>你</strong><div class="mytxt">' + string + '</div></li>'));
                 dataArea.val('').focus();
                 setScrollBar();
             }
-        })
+        });
 
         dataArea.keydown(function(e){
             if(e.ctrlKey && e.keyCode == 13){
                 sendBtn.click();
             }
-        })
+        });
 
         function setScrollBar(){
             var h = chatList.outerHeight() - chatContent.outerHeight() + 20;
             chatContent.scrollTop(h > 0 ? h : 0);
         }
     }
-})(jQuery,io)
+})(jQuery,io);
